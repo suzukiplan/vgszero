9d866510ff3923a1189f59bdc7f21e5af43c4091
suzukiplan <suzukiplan.ys@gmail.com>
2025-09-09 10:18:48 +0900

    バッファーフリップに近い描画モードを追加

--- src/sdl2/vgs0sdl2.cpp
+++ src/sdl2/vgs0sdl2.cpp
@@ -538,6 +538,7 @@ int main(int argc, char* argv[])
     memset(msxKeyCodeMap, 0, sizeof(msxKeyCodeMap));
     bool stabled = false;
     unsigned char key1 = 0;
+    vgs0.vdp->setScanlineMode(false);
     while (!halt) {
         loopCount++;
         auto start = std::chrono::system_clock::now();
@@ -587,6 +588,7 @@ int main(int argc, char* argv[])
                     case SDLK_r: {
                         log("Reset");
                         vgs0.reset();
+                        vgs0.vdp->setScanlineMode(false);
                         break;
                     }
                 }
