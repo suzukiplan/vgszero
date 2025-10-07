572cbb305531e070493727dbc384dcc5dc34a892
suzukiplan <suzukiplan.ys@gmail.com>
2025-10-07 22:20:21 +0900

    Interlace Modeのdefineはあっても良いので戻す

--- lib/sdcc/vgs0lib.h
+++ lib/sdcc/vgs0lib.h
@@ -215,6 +215,12 @@
  */
 #define VGS0_ADDR_PTN1024 ((uint8_t*)0x9F0B)
 
+/** @def
+ * Interlace Mode
+ * https://github.com/suzukiplan/vgszero/blob/master/README.md#interlace-mode
+ */
+#define VGS0_ADDR_INTERLACE ((uint8_t*)0x9F0C)
+
 //! stdint compatible (8bit unsigned)
 typedef unsigned char uint8_t;
 
